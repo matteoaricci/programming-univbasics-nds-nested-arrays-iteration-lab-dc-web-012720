3ab4da6d21ff7a2568f388239c7634c26c354a09
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-05 23:01:04 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,9 +9,6 @@ def join_ingredients(src)
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
   count = 0 
   arr = []
   while count < src.length do 
